7bff058c8a34c18f472d88d772df54430890093f
Sanskar95 <snskr95@gmail.com>
2021-05-24 03:24:11 +0530

    Some changes

--- Assignment_3/fox.c
+++ Assignment_3/fox.c
@@ -1,12 +1,3 @@
-/**
- * Implementing dense matrix multiplication in
- * parallel distributed memory environment.
- *
- * Implementation of fox algorithm in MPI.
- *
- * Author: Paolo Boschini
- * Author: Marcus Ihlar
- */
 
 #include <mpi.h>
 #include <stdio.h>
@@ -16,31 +7,30 @@
 #include <assert.h>
 
 
-// prototypes
 void print_matrix(double*,int);
 void fill_matrix(double*,int,int);
-void fill_identity_matrix(double*,int);
+void generateIdentityMatrix(double*,int);
 void multiply(double*,double*,double*,int);
-void generate(double**, double**, double**, int);
-void distribute(double*, double*, int, int,MPI_Datatype*,  MPI_Comm, MPI_Request*);
-int assert_matrix(double* , double*, int);
+void generateInitialMatrices(double**, double**, double**, int);
+void distributedMatrixToProcesses(double*, double*, int, int,MPI_Datatype*,  MPI_Comm, MPI_Request*);
+int assertMatrices(double* , double*, int);
 
 int main (int argc, char *argv[]) {
 
-  int wrank, nproc;
+  int parentCommunicatorRank, numberOfProcesses;
   double start, end;
 
   MPI_Init(&argc, &argv);               /* initialize mpi               */
-  MPI_Comm_size(MPI_COMM_WORLD, &nproc); /* get the number of processors */
-  MPI_Comm_rank(MPI_COMM_WORLD, &wrank); /* Get my number                */
+  MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses); /* get the number of processors */
+  MPI_Comm_rank(MPI_COMM_WORLD, &parentCommunicatorRank); /* Get my number                */
 
-  if(argc < 2 && wrank == 0) {
+  if(argc < 2 && parentCommunicatorRank == 0) {
     printf("Usage: mpirun -np 4 ./fox n (where n is the matrix size)\n");
     MPI_Finalize(); 
     return -1;
   }
 
-  if(argc < 2 && wrank != 0) {
+  if(argc < 2 && parentCommunicatorRank != 0) {
     MPI_Finalize(); 
     return -1;
   }
@@ -48,17 +38,7 @@ int main (int argc, char *argv[]) {
   double a; int b;
   a = sqrt(atoi(argv[1]));
   b = a;
-  /*    if(a != (double)b && wrank == 0) {
-        printf("n must be perfect square\n");
-        MPI_Finalize(); 
-        return -1;
-	}
-
-	if(a != (double)b && wrank != 0) {
-        MPI_Finalize(); 
-        return -1;
-	}*/
-
+  
   int n = atoi(argv[1]);
 
   MPI_Status stat;
@@ -70,7 +50,7 @@ int main (int argc, char *argv[]) {
   MPI_Status status;
 
   // 2d topology
-  int ndim=2,dims[2]={0,0};
+  int numberOfDimensions=2,dimensions[2]={0,0};
 
   // coordinates for each proc
   int coords[2];
@@ -83,14 +63,14 @@ int main (int argc, char *argv[]) {
 
   /* Create a virtual 2D-grid topology */
   // Creates a division of processors in a cartesian grid
-  MPI_Dims_create(nproc,ndim,dims);
+  MPI_Dims_create(numberOfProcesses,numberOfDimensions,dimensions);
   // Makes a new communicator to which topology information has been attached
-  MPI_Cart_create(MPI_COMM_WORLD,ndim,dims,periods,reorder,&proc_grid);
+  MPI_Cart_create(MPI_COMM_WORLD,numberOfDimensions,dimensions,periods,reorder,&proc_grid);
   // Determines the rank of the calling process in the communicator, Note: use proc_grid
   MPI_Comm_rank(proc_grid,&myid);
 
   // number of blocks on one row or col
-  int pn = dims[0];
+  int pn = dimensions[0];
   printf("PN is %d ", pn);
 
   int blocksize = n*n/(pn*pn);
@@ -104,17 +84,17 @@ int main (int argc, char *argv[]) {
 
   // generate matrices on p0
   if(myid == 0) {
-    generate(&A,&B,&C, n);
+    generateInitialMatrices(&A,&B,&C, n);
     start = MPI_Wtime();
-    distribute(A,B, n, pn, &newtype,  proc_grid, &request);
+    distributedMatrixToProcesses(A,B, n, pn, &newtype,  proc_grid, &request);
   }
 
   //  printf ("grank: %d, myid: %d\n",grank,myid);
 
   // Determines  process  coords  in cartesian topology given rank in group
-  MPI_Cart_coords(proc_grid,myid,ndim,coords);
-  MPI_Recv(Ab, blocksize, MPI_DOUBLE, 0, 111, proc_grid, &status);
-  MPI_Recv(Bb, blocksize, MPI_DOUBLE, 0, 222, proc_grid, &status);
+  MPI_Cart_coords(proc_grid,myid,numberOfDimensions,coords);
+  MPI_Recv(Ab, blocksize, MPI_DOUBLE, 0, 1, proc_grid, &status);
+  MPI_Recv(Bb, blocksize, MPI_DOUBLE, 0, 2, proc_grid, &status);
     
   // print_matrix(Ab, n/pn);
   // print_matrix(Bb, pn);
@@ -140,8 +120,8 @@ int main (int argc, char *argv[]) {
     //print_matrix(Ab_temp, n/pn);
     MPI_Bcast(Ab_temp, blocksize, MPI_DOUBLE, m, proc_row);
     // shifting
-    MPI_Irecv(Bb_temp, blocksize, MPI_DOUBLE, colrank == pn-1 ? 0 : colrank+1, 333, proc_col, &req1);
-    MPI_Isend(Bb, blocksize, MPI_DOUBLE, colrank == 0 ? pn-1 : colrank-1, 333, proc_col, &req2);
+    MPI_Irecv(Bb_temp, blocksize, MPI_DOUBLE, colrank == pn-1 ? 0 : colrank+1, 3, proc_col, &req1);
+    MPI_Isend(Bb, blocksize, MPI_DOUBLE, colrank == 0 ? pn-1 : colrank-1, 3, proc_col, &req2);
     multiply(Ab_temp, Bb, Cb, n/pn);
     MPI_Wait(&req1, &status);
     MPI_Wait(&req2, &status);
@@ -154,20 +134,20 @@ int main (int argc, char *argv[]) {
   // print_matrix(Cb, n/pn);
 
 
-  // Collect blocks of Cb
-  MPI_Isend(Cb, blocksize, MPI_DOUBLE, 0, 444, proc_grid, &request);
+  // Assembly of individual tiles into the parent matrix taking place
+  MPI_Isend(Cb, blocksize, MPI_DOUBLE, 0, 4, proc_grid, &request);
   if(myid == 0) {
     for(i = 0; i < pn*pn; ++i) {
-      MPI_Probe(MPI_ANY_SOURCE, 444, proc_grid, &status);
-      MPI_Cart_coords(proc_grid,status.MPI_SOURCE,ndim,coords);
-      MPI_Recv(&C[(n*(n*coords[0]+coords[1]))/pn], 1, newtype, status.MPI_SOURCE, 444, proc_grid, &status);
+      MPI_Probe(MPI_ANY_SOURCE, 4, proc_grid, &status);
+      MPI_Cart_coords(proc_grid,status.MPI_SOURCE,numberOfDimensions,coords);
+      MPI_Recv(&C[(n*(n*coords[0]+coords[1]))/pn], 1, newtype, status.MPI_SOURCE, 4, proc_grid, &status);
       end = MPI_Wtime();
 	 
     }   
     // printf("%d, %d, %f\n", n, pn*pn, end-start);
     printf("Result Matrix C is:\n");
     multiply(A, B, expectedC, n);
-    if(assert_matrix(C, expectedC, n)==1){
+    if(assertMatrices(C, expectedC, n)==1){
       printf("Test Case Passed!! \n");
     }else{
       printf("Test Case Failed! \n");
@@ -195,10 +175,9 @@ void fill_matrix(double *m, int n, int seed) {
   for (row=0; row<n;row++)
     for (col=0; col<n;col++)
       m[row*n+col] = ((double)rand()*1000/(double)(RAND_MAX));
-  // m[row*n+col]=1;
 }
 
-void fill_identity_matrix(double *m, int n) {
+void generateIdentityMatrix(double *m, int n) {
   int row, col;
   for (row=0; row<n;row++)
     for (col=0; col<n;col++)
@@ -226,7 +205,7 @@ void multiply(double *a,double *b, double *c, int n) {
 	c[i*n+j] += a[i*n+k]*b[k*n+j];
 }
 
-int assert_matrix(double *a,double *b, int n ) {
+int assertMatrices(double *a,double *b, int n ) {
   int row, col;
   int flag = 1;
   for(row = 0; row < n; ++row)
@@ -243,7 +222,7 @@ int assert_matrix(double *a,double *b, int n ) {
 
 
 
-void generate(double** A, double** B, double** C, int n){
+void generateInitialMatrices(double** A, double** B, double** C, int n){
   *A=(double *)calloc(n*n,sizeof(double));
   fill_matrix(*A, n, 23);
   printf("\nMatrix A is:\n");
@@ -251,16 +230,16 @@ void generate(double** A, double** B, double** C, int n){
 
   *B=(double *)calloc(n*n,sizeof(double));
   // fill_matrix(*B, n, 101);
-  fill_identity_matrix(*B,n);
+  generateIdentityMatrix(*B,n);
   printf("Matrix B is:\n");
   print_matrix(*B, n);
 
   *C=(double *)calloc(n*n,sizeof(double));
 }
 
-void distribute(double* A, double* B, int n, int pn, MPI_Datatype *newtype,  MPI_Comm proc_grid, MPI_Request *request){
-  int count=n/pn; int blocklen=n/pn; int stride=n; int grank;
-  MPI_Type_vector(count,blocklen,stride,MPI_DOUBLE,newtype);
+void distributedMatrixToProcesses(double* A, double* B, int n, int pn, MPI_Datatype *newtype,  MPI_Comm proc_grid, MPI_Request *request){
+  int count=n/pn; int blockLength=n/pn; int stride=n; int grank;
+  MPI_Type_vector(count,blockLength,stride,MPI_DOUBLE,newtype);
   MPI_Type_commit(newtype);
   int pos[2], i, j;
    	
@@ -268,8 +247,8 @@ void distribute(double* A, double* B, int n, int pn, MPI_Datatype *newtype,  MPI
     for(j = 0; j < pn; ++j) {
       pos[0] = i; pos[1] = j;
       MPI_Cart_rank(proc_grid,pos,&grank);
-      MPI_Isend(&A[(n*(n*i+j))/pn], 1, *newtype, grank, 111, proc_grid, request);
-      MPI_Isend(&B[(n*(n*i+j))/pn], 1, *newtype, grank, 222, proc_grid, request);
+      MPI_Isend(&A[(n*(n*i+j))/pn], 1, *newtype, grank, 1, proc_grid, request);
+      MPI_Isend(&B[(n*(n*i+j))/pn], 1, *newtype, grank, 2, proc_grid, request);
     }
   }
 }
